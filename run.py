9cf24089ef17e6fbd06456420c5b40d07e4704fd
Walid <walid@example.com>
2026-04-22 03:54:22 +0200

    تجهيز المشروع للنشر على Render وإضافة gunicorn وتحديث المنافذ

--- run.py
+++ run.py
@@ -79,4 +79,6 @@ def init_db():
 if __name__ == '__main__':
     create_folders()
     init_db()
-    app.run(host='0.0.0.0', port=8080, debug=True) 
+    # جلب المنفذ من المتغيرات البيئية أو استخدام 8080 كافتراضي
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True) 
